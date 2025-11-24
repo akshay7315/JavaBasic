2e5360cd337b711a69c51123f21a4ae80df611a8
Akshay Lande <128912334+akshay7315@users.noreply.github.com>
2025-11-24 14:50:29 +0530

    basic.java

---
+++ basic.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SumOfDigit{
+    public static void main(String args[]){
+        int num = 12345;
+        int sum =0;
+         while (num!=0){
+             sum = sum + num % 10;
+             num = num / 10;
+         }
+        System.out.println("sum of all Number: " + sum);
+    }
+}
